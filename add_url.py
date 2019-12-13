b63b86cea38999dd2a9fd1c45f13d44ee7d10da0
Param Thakkar <pht0104@gmail.com>
2019-12-12 21:43:48 -0500

    Simplify if statement

--- add_url.py
+++ add_url.py
@@ -6,13 +6,12 @@ repo = github.get_repo(os.environ["GITHUB_REPOSITORY"])
 open_issues = repo.get_issues(state="open")
 
 for issue in open_issues:
-	if issue.title == "Add URL":
-		if issue.user.login == "paramt":
-			issue.body = "test --> https://www.param.me/test"
-			with open("redirects.csv", "a") as csv:
-				line = issue.body.replace(" --> ", ",")
-				csv.write(line)
-				csv.write("\n")
+	if issue.title == "Add URL" and issue.user.login == "paramt":
+		issue.body = "test --> https://www.param.me/test"
+		with open("redirects.csv", "a") as csv:
+			line = issue.body.replace(" --> ", ",")
+			csv.write(line)
+			csv.write("\n")
 
-			issue.create_comment("The redirect has been added! Please allow up to 5 minutes for the changes to take place.")
-			issue.edit(state="closed")
+		issue.create_comment("The redirect has been added! Please allow up to 5 minutes for the changes to take place.")
+		issue.edit(state="closed")
