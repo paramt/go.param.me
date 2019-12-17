ce6abc4a130f55ba3727d904eed77d551ae44c27
Param Thakkar <pht0104@gmail.com>
2019-12-17 16:09:30 -0500

    Add the ability to remove a redirect

--- add_url.py
+++ add_url.py
@@ -15,3 +15,15 @@ if issue.title == "Add URL" and issue.user.login == "paramt":
 
 	issue.create_comment("The redirect has been added!")
 	issue.edit(state="closed")
+
+if issue.title == "Remove URL" and issue.user.login == "paramt":
+	with open("redirects.csv", "r") as csv:
+		lines = csv.readlines()
+
+	with open("redirects.csv", "w") as csv:
+		for line in csv:
+			if line.split(",")[0] != issue.body:
+				csv.write(line)
+
+	issue.create_comment("The redirect has been removed!")
+	issue.edit(state="closed")
