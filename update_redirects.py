d3a9662738ee52b731a6073e0bc9e0b4828b1998
Param Thakkar <pht0104@gmail.com>
2019-12-19 18:42:02 -0500

    Check for "update redirects" label in script

--- update_redirects.py
+++ update_redirects.py
@@ -7,30 +7,31 @@ github = Github(os.environ["TOKEN"])
 repo = github.get_repo(os.environ["GITHUB_REPOSITORY"])
 issue = repo.get_issue(number=int(os.environ["ISSUE"]))
 
-if issue.title == "Add URL" and issue.user.login == "paramt":
-	with open("redirects.csv", "a") as csv:
-		line = issue.body.replace(" --> ", ",")
-		csv.write(line)
-		csv.write("\n")
+if issue.user.login == "paramt" and issue.get_labels()[0].name == "update redirects":
+	if issue.title == "Add URL":
+		with open("redirects.csv", "a") as csv:
+			line = issue.body.replace(" --> ", ",")
+			csv.write(line)
+			csv.write("\n")
 
-	issue.create_comment("The redirect has been added!")
-	issue.edit(state="closed")
+		issue.create_comment("The redirect has been added!")
+		issue.edit(state="closed")
 
-if issue.title == "Remove URL" and issue.user.login == "paramt":
-	removed = False
+	if issue.title == "Remove URL":
+		removed = False
 
-	with open("redirects.csv", "r") as csv:
-		lines = csv.readlines()
+		with open("redirects.csv", "r") as csv:
+			lines = csv.readlines()
 
-	with open("redirects.csv", "w") as csv:
-		for line in lines:
-			if line.split(",")[0] != issue.body:
-				csv.write(line)
-			else:
-				removed = True
+		with open("redirects.csv", "w") as csv:
+			for line in lines:
+				if line.split(",")[0] != issue.body:
+					csv.write(line)
+				else:
+					removed = True
 
-	if removed:
-		issue.create_comment(f"The redirect `go.param.me/{issue.body}` has been removed!")
-	else:
-		issue.create_comment(f"The redirect `go.param.me/{issue.body}` doesn't exist!")
-	issue.edit(state="closed")
+		if removed:
+			issue.create_comment(f"The redirect `go.param.me/{issue.body}` has been removed!")
+		else:
+			issue.create_comment(f"The redirect `go.param.me/{issue.body}` doesn't exist!")
+		issue.edit(state="closed")
