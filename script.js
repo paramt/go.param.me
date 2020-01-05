e20d872cfff406694feedf898f520d535496141a
Param Thakkar <pht0104@gmail.com>
2020-01-05 09:52:25 -0500

    Fix script.js indents

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 function getCSV(file, callback){
-  var xhttp = new XMLHttpRequest();
+	var xhttp = new XMLHttpRequest();
 
-  xhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-      data = Papa.parse(this.responseText).data;
-      callback(data);
-    }
-  };
+	xhttp.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			data = Papa.parse(this.responseText).data;
+			callback(data);
+		}
+	};
 
-  xhttp.open("GET", file, true);
-  xhttp.send();
+	xhttp.open("GET", file, true);
+	xhttp.send();
 }
 
 function redirect(data){
-  utm = "?utm_source=go.param.me&utm_campaign=redirect";
-  var currentURL = window.location.pathname.substr(1);
-  var redirectURL = "";
+	utm = "?utm_source=go.param.me&utm_campaign=redirect";
+	var currentURL = window.location.pathname.substr(1);
+	var redirectURL = "";
 
-  for(var i = 1; i < data.length - 1; i++){
-    shortURL = data[i][0];
-    longURL = data[i][1];
+	for(var i = 1; i < data.length - 1; i++){
+		shortURL = data[i][0];
+		longURL = data[i][1];
 
-    if(currentURL == shortURL){
-      redirectURL = longURL;
-	  console.log("Setting redirect URL to " + longURL);
-	  utm += "&utm_medium=short-url";
-    }
-  }
+		if(currentURL == shortURL){
+			redirectURL = longURL;
+			console.log("Setting redirect URL to " + longURL);
+			utm += "&utm_medium=short-url";
+		}
+	}
 
-  if(redirectURL === ""){
-    redirectURL = "https://www.param.me";
-	console.log("Invalid URL! Defaulting redirect URL to param.me");
-	utm += "&utm_medium=catch-all";
-  }
+	if(redirectURL === ""){
+		redirectURL = "https://www.param.me";
+		console.log("Invalid URL! Defaulting redirect URL to param.me");
+		utm += "&utm_medium=catch-all";
+	}
 
-  console.log("Redirecting");
-  window.location = redirectURL + utm;
+	console.log("Redirecting");
+	window.location = redirectURL + utm;
 }
 
 getCSV("https://raw.githubusercontent.com/paramt/go.param.me/master/redirects.csv", redirect)
